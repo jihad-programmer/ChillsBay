c2c897424e5cfbb90b8b79aaa9fe581c3424b2e5
jihad-programmer <mdjaminur2300@gmail.com>
2024-12-01 23:56:30 -0800

    Naavbar setup done

--- ChillSbay/src/App.jsx
+++ ChillSbay/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-
+import MyNavbar from './components/Navbar'
 
 function App() {
    return (
     <>
-      
+      <MyNavbar/>
       
     </>
   )
---
+++ ChillSbay/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {
+    Navbar, 
+    NavbarBrand, 
+    NavbarContent, 
+    NavbarItem, 
+    NavbarMenuToggle,
+    NavbarMenu,
+    NavbarMenuItem,
+    Link,
+  } from "@nextui-org/react";
+
+  import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@nextui-org/react";
+
+
+
+function MyNavbar() {
+  return (
+    <>
+    
+
+    </>
+  )
+}
+
+export default MyNavbar
