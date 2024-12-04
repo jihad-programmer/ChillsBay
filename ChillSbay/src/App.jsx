6e392940509564124077d702da7e5b9e5a60c4f3
jihad-programmer <mdjaminur2300@gmail.com>
2024-12-03 16:17:59 -0800

    ImgSection + ThingsToDo All card Done

--- ChillSbay/src/App.jsx
+++ ChillSbay/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import HeroArea from './components/HeroArea'
+import ImgSection from './components/ImgSection'
 import MyNavbar from './components/Navbar'
+import ThingsToDo from './components/ThingsToDo'
 
 function App() {
    return (
     <>
       <MyNavbar/>
       <HeroArea/>
+      <ImgSection/>
+      <ThingsToDo/>
       
     </>
   )
